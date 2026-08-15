383853bfe289707db900ab663dd745dc8027cda2
xtapo <40712625+xtapo@users.noreply.github.com>
2026-08-15 08:21:45 +0700

    style(friends): đồng bộ soft-UI xanh lá cho friend-list.vue

--- frontend/src/components/friends/friend-list.vue
+++ frontend/src/components/friends/friend-list.vue
@@ -20,9 +20,9 @@
         class="px-2"
       >
         <template #prepend>
-          <v-avatar color="blue-grey-lighten-4" size="40">
+          <v-avatar color="grey-lighten-3" size="40">
             <v-img v-if="friend.avatar" :src="friend.avatar" />
-            <v-icon v-else color="blue-grey">mdi-account</v-icon>
+            <v-icon v-else color="grey-darken-1">mdi-account</v-icon>
           </v-avatar>
         </template>
 
